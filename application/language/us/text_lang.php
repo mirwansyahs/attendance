874574417bd2ae6f5012b96a71e4a7b2f0273293
matczdayat <matczdayat@gmail.com>
2023-04-30 23:37:52 -0700

    Ubah text_lang.php

--- application/language/us/text_lang.php
+++ application/language/us/text_lang.php
@@ -6,10 +6,122 @@ $lang['simpan'] = 'Save';
 $lang['ubah'] = 'Update';
 $lang['hapus'] = 'Delete';
 $lang['tambah_data'] = 'Add Data';
+$lang['buka_maps'] = 'Open Map';
 
-$lang['human_capital']  = 'Human Capital';
-$lang['employee'] = 'Employee';
-$lang['employee_name']  = 'Employee Name';
-$lang['employee_identification'] = 'Identification Number';
-$lang['employee_personal_email'] = 'Personal Email';
-$lang['employee_phone'] = 'Phone Number';
+// Module
+$lang['human_capital']          = 'Human Capital';
+$lang['core_management']        = 'Core Management';
+$lang['finance']                = 'Finance';
+
+// Employee
+$lang['employee']                               = 'Employee';
+$lang['tambah_employee']                        = 'Add Employee';
+$lang['ubah_employee']                          = 'Update Employee';
+$lang['employee_name']                          = 'Employee Name';
+$lang['employee_identification']                = 'Identification Number';
+$lang['employee_personal_email']                = 'Personal Email';
+$lang['employee_phone']                         = 'Handphone Number';
+$lang['employee_first_name']                    = 'First Name';
+$lang['employee_middle_name']                   = 'Middle Name';
+$lang['employee_last_name']                     = 'Last Name';
+$lang['employee_identification_number']         = 'NIK';
+$lang['employee_family_identification_number']  = 'No. KK';
+$lang['employee_position']                      = 'Employee Position';
+$lang['employee_personal_email']                = 'Employee Personal Email';
+$lang['employee_corporate_email']               = 'Employee Corporate Email';
+$lang['employee_password']                      = 'Employee Password';
+$lang['employee_phone']                         = 'Employee Phone';
+$lang['employee_religion']                      = 'Employee Religion';
+$lang['employee_work_status']                   = 'Employee Work Status';
+$lang['employee_marital_status']                = 'Employee Marital Status';
+$lang['employee_tenant']                        = 'Employee Tenan';
+
+
+// Tenant
+$lang['tenant']                                 = 'Tenant';
+$lang['tambah_tenant']                          = 'Add Tenan';
+$lang['ubah_tenant']                            = 'Update Tenant';
+$lang['tenant_name']                            = 'Tenant Name';
+$lang['tenant_location']                        = 'Tenant Location';
+$lang['tenant_modul']                           = 'Tenant Module';
+$lang['jadikan_core']                           = 'Assign as Core';
+$lang['core']                                   = 'Core';
+$lang['tenant_see_location']                    = 'View Tenant Location';
+$lang['tenant_see_modul']                       = 'View Tenant Module';
+
+
+// Tenant Location
+$lang['tenant_location']            = 'Tenant Location';
+$lang['tambah_tenant_location']     = 'Add Tenant Location';
+$lang['ubah_tenant_location']       = 'Update Tenant Location';
+$lang['tenant_see_location']        = 'View Tenant Location';
+$lang['tenant_location_name']       = 'Location Name';
+$lang['tenant_location_address']    = 'Location Address';
+$lang['tenant_location_latitude']   = 'Latitude';
+$lang['tenant_location_longitude']  = 'Longitude';
+$lang['tenant_location_radius']     = 'Location Radius ';
+
+
+// Tenant Setting Modul
+$lang['tenant_setting_modul']           = 'Tenant Module';
+$lang['tambah_tenant_setting_modul']    = 'Add Tenant Module';
+$lang['ubah_tenant_modul']              = 'Update Tenant Module';
+
+
+// Role
+$lang['role']             = 'Role';
+$lang['tambah_role']      = 'Add Role';
+$lang['ubah_role']        = 'Update Role';
+$lang['role_name']        = 'Role Name';
+
+
+// Modul
+$lang['modul']             = 'Module';
+$lang['tambah_modul']      = 'Add Module';
+$lang['ubah_modul']        = 'Update Module';
+$lang['modul_name']          = 'Module Name';
+$lang['modul_url']          = 'Module URL';
+$lang['modul_icon']          = 'Module Icon';
+
+
+// TimeProfile
+$lang['time_profile']             = 'Time Profile';
+$lang['tambah_time_profile']      = 'Add Time Profile';
+$lang['ubah_time_profile']        = 'Update TIme Profile';
+$lang['time_profile_name']        = 'Time Profile Name';
+$lang['time_name']                = 'Time Name';
+$lang['time_start']               = 'Start Time ';
+$lang['time_end']                 = 'End Time';
+$lang['shift']                    = 'Shift';
+$lang['no_profile_shift']         = 'No shift Profile';
+$lang['see_profile_shift']        = 'View shift  profile';
+$lang['time']                     = 'Time';
+
+
+// ShiftProfile
+$lang['shift']                    = 'Shift';
+$lang['tambah_shift']             = 'Add Shift Profile';
+$lang['ubah_shift']               = 'Update Shift Profile';
+$lang['shift_name']               = 'Profil Waktu';
+$lang['shift_name']              = 'Nama waktu';
+$lang['shift_start']             = 'Waktu mulai';
+$lang['shift_end']               = 'Waktu selesai';
+$lang['shift']                   = 'Shift';
+$lang['no_profile_shift']        = 'Profile shift tidak ada';
+$lang['see_profile_shift']       = 'Lihat profile shift';
+
+$lang['attendance']              = 'Kehadiran';
+
+$lang['hse']                     = "HSE";
+$lang['site_management']         = "Site Management";
+$lang['sc_management']           = "SC Management";
+$lang['finance']                 = "keuangan";
+$lang['mining_operation']        = "Mining Operation";
+$lang['assets_management']       = "Asset Management";
+$lang['correspondency']          = "Correspondency";
+$lang['purchasing_management']   = "Purchasing Management";
+$lang['hr_management']           = "HR Management";
+$lang['cr_management']           = "CR Management";
+$lang['warehouse_management']    = "Warehouse Management";
+
+?>
